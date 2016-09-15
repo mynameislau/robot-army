1415ef28f0ed8405bf7609ccd735f007c08f3420
Laurent Falda <l.falda@groupe-scala.com>
2016-09-15 11:44:18 +0200

    Simple two way data binding

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,6 +1,24 @@
 import { Component } from '@angular/core';
+
+interface IRobot {
+  name:string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+  template: `
+  <article>
+    <h1>{{robot.name}}</h1>
+    <figure>
+      <img src="https://robohash.org/{{robot.name}}.png">
+    </figure>
+    <p>{{robot.goal}}</p>
+  </article>
+  <input [(ngModel)]="robot.name" placeholder="name"/>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  robot:IRobot = {
+    name: 'Bender'
+  }
+};
